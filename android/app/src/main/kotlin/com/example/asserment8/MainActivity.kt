085f8e1043470914444683bacc9c713f87c1a1c4
BappyGuria <guriabappye8@gmail.com>
2023-09-30 22:17:19 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/asserment8/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.asserment8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/MyHomePage.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Profile'),
+        ),
+        body: OrientationBuilder(
+          builder: (BuildContext context, Orientation orientation) {
+            if(orientation == Orientation.portrait){
+              return portraitMode(context);
+            }else{
+              return landscapeMode(context);
+            };
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+portraitMode (context){
+  return SingleChildScrollView(
+    scrollDirection: Axis.vertical,
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          SizedBox(height: 10,),
+          Center(child: profileCircleAvatar()),
+          SizedBox(height: 10,),
+          Text('John Doe',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+          SizedBox(height: 10,),
+          Wrap(
+            children: [
+              Text('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',style: TextStyle(
+                  fontSize: 20
+              ),)
+
+            ],
+          ),
+          SizedBox(height: 20,),
+          profileOrientationBuilder(context),
+        ],
+      ),
+    ),
+  );
+}
+
+landscapeMode(context){
+  return SingleChildScrollView(
+    scrollDirection:Axis.vertical,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          flex: 4,
+            child: profileCircleAvatar(),
+        ),
+        Expanded(
+          flex: 6,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('John Doe',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+                SizedBox(height: 10,),
+                Wrap(
+                  children: [
+                    Text('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',style: TextStyle(
+                        fontSize: 20
+                    ),)
+
+                  ],
+                ),
+                profileOrientationBuilder(context)
+              ],
+            )
+        )
+      ],
+    ),
+  );
+
+}
+
+
+
+CircleAvatar profileCircleAvatar(){
+  return CircleAvatar(
+    radius: 150,
+    backgroundImage: NetworkImage("https://img.freepik.com/premium-photo/circle-blue-abstract-futuristic-technology-background-technology-design-template-generative-ai_1423-9184.jpg?w=2000"),
+  );
+}
+
+GridView profileOrientationBuilder(context){
+  return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        mainAxisSpacing: 5,
+        crossAxisSpacing: 5,
+      ),
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: 6,
+      itemBuilder:(context, index){
+        return Image.network("https://www.humanresourcing.co.uk/wp-content/uploads/2023/08/connected-technology.jpg", fit: BoxFit.cover,);
+      }
+  );
+}
